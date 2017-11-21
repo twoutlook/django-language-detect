d48246ee8c071925cb65daba703e0ecfcf8f9257
William Marquardt <marquardt@navegg.com>
2017-11-21 16:55:48 -0200

    language detect v1.0

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+version = '1.0.0'
+
+
+setup(
+    name='django-language-detect',
+    version=version,
+    keywords='django-language-detect',
+    description='Django browser language detect',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/wmarquardt/django-language-detect',
+
+    author='William Marquardt',
+    author_email='williammqt@gmail.com',
+
+    packages=['language_detect'],
+    py_modules=[],
+    install_requires=['django-six'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Spreadsheet',
+    ],
+)
